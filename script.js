b09e897042cca75493788a42663128296deb33c8
Ridge <rijocson@hotmail.com>
2022-08-03 14:27:18 -0700

    Feat to compare user and computer choices

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // TODO: Ask if the user wants to play the game
 
-// TODO: Prompt the USER to choose rock paper scissors
 const choices = ["rock", "paper", "scissors"];
+
 // TODO: Store USER selection into variable
 
 // TODO: Create COMPUTER variable
@@ -11,4 +11,30 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 // TODO: Compare USER and COMPUTER choices
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  if (choices.includes(playerSelection)) {
+    if (playerSelection === computerSelection) {
+      return "Draw!";
+    }
+
+    if (playerSelection === "rock") {
+      if (computerSelection === "scissors") {
+        return "You win! Rock beats Scissors!";
+      } else {
+        return "You lose. Rock loses to Paper :(";
+      }
+    } else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+        return "You win! Paper beats Rock!";
+      } else {
+        return "You lose. Paper loses to Scissors :(";
+      }
+    } else if (playerSelection === "scissors") {
+      if (computerSelection === "paper") {
+        return "You win! Scissors beats Paper!";
+      } else {
+        return "You lose. Scissors loses to Rock :(";
+      }
+    }
+  }
+}
