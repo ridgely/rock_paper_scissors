b0ef8195791c86af4176b947c6cdd8bd666be835
Ridge <rijocson@hotmail.com>
2022-08-03 12:58:16 -0700

    Add function to generate computer choice

---
+++ script.js
@@ -0,0 +1,13 @@
+// TODO: Ask if the user wants to play the game
+
+// TODO: Prompt the USER to choose rock paper scissors
+const choices = ["rock", "paper", "scissors"];
+// TODO: Store USER selection into variable
+
+// TODO: Create COMPUTER variable
+
+// TODO: Generate random variable and store it in the COMPUTER variable
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+// TODO: Compare USER and COMPUTER choices
